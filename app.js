a0001d6f0a0136fb8cbd036e5037505d9a749f85
ivan2798 <inge1998@hotmail.com>
2022-09-20 09:18:58 -0500

    objeto

---
+++ app.js
@@ -0,0 +1,23 @@
+let a = new String("Hola");
+console.log(a);
+
+const b = 0;
+console.log(b);
+
+
+const c = new Object();
+console.log(c);
+
+const ivan = {
+        nombre:"Ivan",
+        apellido:"Herrera",
+        idiomas:["ingles","frances","aleman"],
+        soltero:true,
+        contacto:{
+                email:"emial",
+                facebook:"face",
+                movil:"1234567889"
+        }
+
+}
+
